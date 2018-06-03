77884d2fdb8256912d20de8721c44608f5ca1340
Marshall Danel <marshalldanel@gmail.com>
2018-06-03 15:57:26 -0700

    Removed secrets.js, added dotenv into server.js

--- server/server.js
+++ server/server.js
@@ -2,18 +2,19 @@ const express = require('express'),
     bodyParser = require('body-parser'),
     mongoose = require('mongoose'),
     port = process.env.PORT || 5000,
-    secrets = require('./secrets'),
     app = express();
 
-mongoose.connect(secrets.mlab, (err) => {
-    console.log('Connected to mLab')
+require('dotenv').config();
+
+mongoose.connect(process.env.MLAB_URI, (err) => {
+  console.log('Connected to mLab')
 })
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(bodyParser.json())
 
-app.get('/', (req, res) => {
-    res.end();
+app.get('/', (req, res) => {  
+  res.end();
 })
 
 app.listen(port, () => {
