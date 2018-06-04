7cc38d5052d41de98437eff70dcd343642333618
Marco Seoane <marcorseoane@outlook.com>
2018-06-04 21:19:05 +0100

    scss working

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import './styles/css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <h1>
         Find your future favorite band.
-      </div>
+      </h1>
     );
   }
 }
