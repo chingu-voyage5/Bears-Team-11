eb0b1fae37e2b400e9096c8c3f118a2768b542f7
Marco Seoane <marcorseoane@outlook.com>
2018-06-08 16:56:34 +0100

    Navbar initial draft

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,11 @@
 import React, { Component } from 'react';
+import Navbar from './components/Navbar';
 import './styles/css/App.css';
 
 class App extends Component {
   render() {
     return (
-      <h1>
-        Find your future favorite band.
-      </h1>
+      <Navbar />
     );
   }
 }
