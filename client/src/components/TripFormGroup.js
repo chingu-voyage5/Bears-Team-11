c832a05e970fe750a29182f479fcd76b8d78c1ca
Stefan <stefantrysil@gmail.com>
2018-06-14 22:28:32 -0600

    add DateRangePicker component from react-dates npm package

--- client/src/components/TripFormGroup.js
+++ client/src/components/TripFormGroup.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import PlacesAutocomplete from "react-places-autocomplete";
+import { DateRangePicker } from 'react-dates';
+
+import 'react-dates/lib/css/_datepicker.css';
 
 const renderFunc = ({ getInputProps, getSuggestionItemProps, suggestions }) => (
-  <div className="autocomplete-root">
+  <div className="field">
     <input
       {...getInputProps({
         className: "input is-primary",
         type: "text"
       })}
     />
+    
     <div className="autocomplete-dropdown-container">
       {suggestions.map(suggestion => {
         const className = suggestion.active ? 'has-background-primary' : 'has-background-white';
@@ -24,13 +28,49 @@ const renderFunc = ({ getInputProps, getSuggestionItemProps, suggestions }) => (
 
 const TripFormGroup = (props) => {
   return (
-    <div>
-      <PlacesAutocomplete
-        value={props.address}
-        onChange={address => props.handleAddressChange(props.id, address)}
-      >
-        {renderFunc}
-      </PlacesAutocomplete>
+    
+    <div className="field is-horizontal">
+      <div className="field-body">
+
+        <PlacesAutocomplete
+          value={props.address}
+          onChange={address => props.handleAddressChange(props.id, address)}
+        >
+          {renderFunc}
+        </PlacesAutocomplete>
+
+        <DateRangePicker
+          startDate={props.startDate}
+          startDateId={`startDate ${props.id}`}
+          endDate={props.endDate}
+          endDateId={`endDate ${props.id}`}
+          onDatesChange={({ startDate, endDate }) => props.handleDateChange(props.id, startDate, endDate)}
+          focusedInput={props.focusedDateInput}
+          onFocusChange={focusedInput => props.handleDateInputFocusChange(focusedInput)}
+        />
+        
+      </div>
+      
+      <div className="field is-grouped">
+        <div className="control">
+          <button 
+            type="button"
+            className="button is-link"
+            onClick={props.handleCityAdd}
+          >
+            add
+          </button>
+        </div>
+        <div className="control">
+          <button 
+            type="button"
+            className="button is-danger"
+            onClick={() => props.handleCityRemove(props.id)}
+          >
+            remove
+          </button>
+        </div>
+      </div>
     </div>
   )
 };
