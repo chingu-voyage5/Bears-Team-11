79a84b3fccdd331c905f3b28d9a20bd22ac0b8de
Marshall Danel <marshalldanel@gmail.com>
2018-06-20 20:46:10 -0700

    Clean/Add OAuth success component for popup close

--- client/src/App.js
+++ client/src/App.js
@@ -1,44 +1,19 @@
 import React, { Component } from 'react';
+import { Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
 import Footer from './components/Footer';
 import TripForm from './components/TripForm';
+import AuthSuccess from './AuthSuccess';
 
 import './styles/css/App.css';
 import 'react-dates/initialize';
 
-
 class App extends Component {
-  constructor() {
-    super();
-    const params = this.getHashParams();
-    this.state = {
-      loggedIn: params.access_token ? true : false
-    }
-  }
-  getHashParams() {
-    let hashParams = {};
-    let e, r = /([^&;=]+)=?([^&;]*)/g,
-      q = window.location.hash.substring(1);
-    e = r.exec(q)
-    while (e) {
-      hashParams[e[1]] = decodeURIComponent(e[2]);
-      e = r.exec(q);
-    }
-    return hashParams;
-  }
-  componentWillMount() {
-    let test = String(window.location.href).split('&')
-    test.splice(0, 1)
-    if (test.length > 1) {
-      const accessToken = test[0].split('=')[1]
-      const refreshToken = test[1].split('=')[1]
-    }
-    console.log(process.env)
-  }
   render() {
     return (
       <div>
+        <Route path="/success" component={AuthSuccess} />
         <Navbar />
         <Hero />
         <TripForm />
