c8943ec1c4a381cc4856bd2ec3695e131596cb35
Marshall Danel <marshalldanel@gmail.com>
2018-06-20 20:47:13 -0700

    Initial OAuth button connect

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,22 +1,31 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from '../assets/logo1.png';
 import spotify from '../assets/spotify.png';
 import '../styles/css/Navbar.css';
 
-const Navbar = () => {
+class Navbar extends Component {
+  oAuth = () => {
+    const url = '/login';
+    const name = '_blank';
+    const specs = 'width=500,height=500';
+    window.open(url, name, specs);
+  }
+  
+  render() {
     return (
-        <div className="navbar" role="navigation" aria-label="main navigation">
-            <div className="navbar-brand">
-                <a className="navbar-item" href="/">
-                    <img src={logo} alt="Bears Events" width="40" height="40"></img>
-                    <h3>BEARS·EVENTS</h3>
-                </a>
-            </div>
-            <a>
-                <img src={spotify} alt="Bears Events" width="40" height="40"></img>
-            </a>
+      <div className="navbar" role="navigation" aria-label="main navigation">
+        <div className="navbar-brand">
+          <a className="navbar-item">
+            <img src={logo} alt="Bears Events" width="40" height="40"></img>
+            <h3>BEARS·EVENTS</h3>
+          </a>
         </div>
+        <a onClick={this.oAuth}>
+          <img src={spotify} alt="Bears Events" width="40" height="40"></img>
+        </a>
+      </div>
     )
+  }
 }
 
 export default Navbar
