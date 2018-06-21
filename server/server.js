f1c470d9753632bd52293f80856231cd3e16c638
Marshall Danel <marshalldanel@gmail.com>
2018-06-20 20:40:44 -0700

    Add express-session, passport + basic server pkgs

--- server/server.js
+++ server/server.js
@@ -1,68 +1,37 @@
 const express = require('express'),
-  bodyParser = require('body-parser'),
-  mongoose = require('mongoose'),
-  port = process.env.PORT || 5000,
-  cors = require('cors'),
-  cookieParser = require('cookie-parser'),
-  querystring = require('querystring'),
-  Spotify = require('spotify-web-api-node'),
-  app = express();
+      morgan = require('morgan');
+      bodyParser = require('body-parser'),
+      mongoose = require('mongoose'),
+      expressSession = require('express-session');
+      MongoStore = require('connect-mongo')(expressSession);
+      passport = require('passport');
+      port = process.env.PORT || 5000,
+      app = express();
 
 require('dotenv').config();
-const CLIENT_ID = process.env.SPOTIFY_CLIENT_ID;
-const CLIENT_SECRET = process.env.SPOTIFY_CLIENT_SECRET;
-const REDIRECT_URI = 'http://localhost:5000/callback';
-const STATE_KEY = 'spotify_auth_state';
-const scopes = ['user-read-private', 'user-read-email'];
 
-const spotifyApi = new Spotify({
-  clientId: CLIENT_ID,
-  clientSecret: CLIENT_SECRET,
-  redirectUri: REDIRECT_URI
-});
-
-const generateRandomString = N => (Math.random().toString(36)+Array(N).join('0')).slice(2, N+2);
+app.use(morgan('dev')); 
 
 mongoose.connect(process.env.MLAB_URI, (err) => {
+  if (err) console.error(err)
   console.log('Connected to mLab')
 })
 
-app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json())
-app.use(cookieParser())
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-  res.end();
-})
+app.set('trust proxy', 1) // secure cookie but served over http
+app.use(expressSession({
+  secret: process.env.SESSION_SECRET,
+  resave: false,
+  saveUninitialized: true,
+  cookie: { httpOnly: true, secure: true },
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+}))
 
-app.get('/login', (_, res) => {
-  const state = generateRandomString(16);
-  res.cookie(STATE_KEY, state);
-  res.send({url: spotifyApi.createAuthorizeURL(scopes, state)});
-});
+app.use(passport.initialize());
+app.use(passport.session());
 
-app.get('/callback', (req, res) => {
-  const { code, state } = req.query;
-  
-  const storedState = req.cookies ? req.cookies[STATE_KEY] : null;
-  // first do state validation
-  if (state === null || state !== storedState) {
-    res.redirect('/#/error/state mismatch');
-  // if the state is valid, get the authorization code and pass it on to the client
-  } else {
-    res.clearCookie(STATE_KEY);
-    // Retrieve an access token and a refresh token
-    spotifyApi.authorizationCodeGrant(code).then(data => {
-      const { expires_in, access_token, refresh_token } = data.body;
-
-      // Set the access token on the API object to use it in later calls
-      spotifyApi.setAccessToken(access_token);
-      spotifyApi.setRefreshToken(refresh_token);
-
-      // use the access token to access the Spotify Web API
-      spotifyApi.getMe().then(({ body }) => {
-        console.log(body);
-      });
 
 const authRoutes = require('./routes/auth');
 app.use(authRoutes());
