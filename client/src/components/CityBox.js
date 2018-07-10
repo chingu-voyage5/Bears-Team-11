24604882e1999bd7697e6dea4f3aeefeb9629724
Stefan <stefantrysil@gmail.com>
2018-07-10 12:47:57 -0600

    handle loading case on CityBox component

--- client/src/components/CityBox.js
+++ client/src/components/CityBox.js
@@ -18,36 +18,42 @@ class CityBox extends Component {
   }
 
   render () {
-    console.log(this.props);
-    return <div>check console for data</div>
-    // return (
-    //   <div>
-    //     <div className='pad-top pad-bottom'>
-    //       <div 
-    //         className='box'
-    //         onClick={() => this.setState({ expanded: !this.state.expanded })}
-    //         >
-    //         <div className='columns'>
-    //           <div className='column has-text-centered'>
-    //             {trip.location}
-    //           </div>
-    //           <div className='column is-1 has-text-centered has-text-danger'>
-    //             <i className='fa fa-chevron-down'></i>
-    //           </div>
-    //           <div className='column has-text-centered'>
-    //             <span>
-    //             {this.handleDateFormat(trip.start_date)} - {this.handleDateFormat(trip.end_date)}
-    //             </span>  
-    //           </div>
-    //         </div>
-    //         {(this.state.expanded && <div>Hi</div>)}
-    //       </div>
-    //     </div>
-    //   </div> 
-    // );
+    if (this.props.data.loading)
+      return <div>Loading...</div>;
+
+    // additional available vars - events, totalEvents
+    const { location } = this.props.data.city;
+    const { startDate, endDate } = this.props.city;
+
+    return (
+      <div>
+        <div className='pad-top pad-bottom'>
+          <div 
+            className='box'
+            onClick={() => this.setState({ expanded: !this.state.expanded })}
+            >
+            <div className='columns'>
+              <div className='column has-text-centered'>
+                {location}
+              </div>
+              <div className='column is-1 has-text-centered has-text-danger'>
+                <i className='fa fa-chevron-down'></i>
+              </div>
+              <div className='column has-text-centered'>
+                <span>
+                {this.handleDateFormat(startDate)} - {this.handleDateFormat(endDate)}
+                </span>  
+              </div>
+            </div>
+            {(this.state.expanded && <div>data successfully loaded</div>)}
+          </div>
+        </div>
+      </div> 
+    );
   }
 }
 
+// only single genre supported atm, not even sure if it is working as expected
 export default graphql(getCityQuery, {
   options: ({ city: { location, startDate, endDate }, genre }) => ({
     variables: {
