b51cf042f2770b873f13c60831f6f640a27b41d4
Marshall Danel <marshalldanel@gmail.com>
2018-07-15 22:20:49 -0700

    Update displayed date format

--- client/src/components/ScrollerEvent.js
+++ client/src/components/ScrollerEvent.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import moment from 'moment';
 import '../styles/css/Event.css'
 
 class ScrollerEvent extends Component {
@@ -8,6 +9,7 @@ class ScrollerEvent extends Component {
 
     }
   }
+
   render(){
     const { name } = this.props;
     const { venue } = this.props;
@@ -17,9 +19,9 @@ class ScrollerEvent extends Component {
     return(
     <div className="box is-marginless" id="concert">
       <div className='columns'>
-          <div className='column has-text-centered is-4'>{name}</div>
+          <div className='column is-4 has-text-centered'>{name}</div>
           <div className='column is-4 has-text-centered'>{venue}</div>
-          <div className='column is-1'>{date}</div>
+          <div className='column is-2 has-text-centered'>{moment(date).format('MMMM Do, h:mm a')}</div>
           <div className='column is-size-7 has-text-centered'>
             <a href={link} target='_blank'>Event Link</a>
           </div>
