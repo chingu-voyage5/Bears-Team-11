52698c3dee22ffc91631d1c14adecd0367b55bff
Stefan <stefantrysil@gmail.com>
2018-07-18 02:14:29 -0600

    add feature citybox loading state

--- client/src/components/CityBox.js
+++ client/src/components/CityBox.js
@@ -1,74 +1,79 @@
-import React, { Component } from 'react';
-import { graphql } from 'react-apollo';
+import React from 'react';
+import { Query } from 'react-apollo';
 import moment from 'moment';
+import classNames from 'classnames';
+
 import Playlist from './Playlist';
 import ConcertScroller from './ConcertScroller';
-
 import { getCityQuery } from '../graphql/queries';
 import "../styles/css/Results.css";
 
-class CityBox extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      expanded: false
-    }
-  }
+const handleDateFormat = (date) => moment(date).format('ll');
 
-  handleDateFormat(date) {
-    return moment(date).format('ll');
-  }
-
-  render () {
-    if (this.props.data.loading)
-      return <div>Loading...</div>;
-
-    // additional available vars - events, totalEvents
-    console.log('this.props.data :', this.props.data);
-    const { location } = this.props.city;
-    const { startDate, endDate } = this.props.city;
-    const events = this.props.data.city.events.filter((e) => e !== undefined && e.performer && e.performer.spotify !== undefined );
-    return (
-      <div>
-        <div className='pad-top pad-bottom'>
-          <div 
-            className='box'
-            onClick={() => this.setState({ expanded: !this.state.expanded })}
-            >
-            <div className='columns'>
-              <div className='column has-text-centered'>
-              <i className="fa fa-map-marker-alt" style={{marginRight: '5px', color: '#FF3860'}}></i>
-                {location}
-              </div>
-              <div className='column is-1 has-text-centered has-text-danger'>
-                <i className='fa fa-chevron-down' style={{color: '#7782E9'}}></i>
-              </div>
-              <div className='column has-text-centered'>
-                <span>
-                {this.handleDateFormat(startDate)} - {this.handleDateFormat(endDate)}
-                </span>  
-              </div>
-            </div>
-            {/* {(this.state.expanded && <div>data successfully loaded</div>)} */}
-          </div>
-        </div>
-        <div>
-          <ConcertScroller events={events} />
-          <Playlist events={events} />
-        </div>
-
-      </div> 
-    );
-  }
-}
-
-export default graphql(getCityQuery, {
-  options: ({ city: { location, startDate, endDate }, genres }) => ({
-    variables: {
+const CityBox = ({ city: { location, startDate, endDate }, genres, id, expandCity, expanded }) => (
+  <Query 
+    query={getCityQuery} 
+    variables={{ 
       location,
-      genres,
       start_date: startDate.format("YYYYMMDD"),
       end_date: endDate.format("YYYYMMDD"),
-    }
-  })
-})(CityBox);
+      genres
+    }}
+  >
+    {({ loading, error, data }) => {
+      console.log(loading, error, data);
+      let events = [];
+      if (!loading && !error) {
+        events = data.city.events.filter(event => event.performer && event.performer.spotify);
+      }
+
+      const buttonClass = classNames('button is-large is-white', {
+        'is-loading': loading
+      });
+
+      const spanClass = classNames('icon', { 'has-text-warning': error, 'has-text-info': !loading && data.city });
+      const iconClass = classNames('fas', {'fa-times': error, 'fa-chevron-down': data.city });
+
+      return ( 
+        <div>
+          <div className='pad-top pad-bottom'>
+            <div 
+              className='box'
+              >
+              <div className='columns'>
+                <div className='column has-text-centered'>
+                <i className="fa fa-map-marker-alt" style={{marginRight: '5px', color: '#FF3860'}}></i>
+                  {location}
+                </div>
+                <button
+                  className={buttonClass}
+                  onClick={() => expandCity(id)}
+                  disabled={loading || error}
+                >
+                  <span className={spanClass}>
+                    <i className={iconClass}></i>
+                  </span>
+                </button>
+                <div className='column has-text-centered'>
+                  <span>
+                  {handleDateFormat(startDate)} - {handleDateFormat(endDate)}
+                  </span>  
+                </div>
+              </div>
+            </div>
+          </div>
+          {
+            expanded && data &&
+            <div>
+              <ConcertScroller events={events} />
+              <Playlist events={events} />
+            </div>
+          }
+        </div>
+      );
+    }}
+    
+  </Query>
+)
+
+export default CityBox;
