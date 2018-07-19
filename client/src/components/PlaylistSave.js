cdfa655d8ccb1c473b08fb9ff588ade1855fb52b
Marshall Danel <marshalldanel@gmail.com>
2018-07-19 15:23:47 -0700

    Add form, no style

--- client/src/components/PlaylistSave.js
+++ client/src/components/PlaylistSave.js
@@ -1,19 +1,62 @@
 import React, { Component } from 'react';
 
 class PlaylistSave extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      playlistName: '',
+      status: 'Public'
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({[event.target.name]: event.target.value})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+  }
+
   render() {
+    console.log(this.state)
     return (
       <div className='box is-radiusless'>
         <div className='columns has-text-centered'>
-          <div className='column'>Playlist Name
-            <div className='column'>Input form</div>
-          </div>
-          <div className='column'>Status
-            <div className='column'>Select form</div>
-          </div>
+          <form onSubmit={this.handleSubmit}>
+            <div>Playlist Name
+              <input type='text' name='playlistName' value={this.state.playlistName} onChange={this.handleChange}/>
+            </div>
+            <div>Status
+              <div className='control'>
+                <label className='radio'>
+                  <input
+                    type='radio'
+                    name='status'
+                    value='Public'
+                    checked={this.state.status === 'Public'}
+                    onChange={this.handleChange}
+                  />
+                  Public
+                </label>
+                <label className='radio'>
+                  <input 
+                  type='radio'
+                  name='status'
+                  value='Private'
+                  checked={this.state.status === 'Private'}
+                  onChange={this.handleChange}
+                  />
+                  Private
+                </label>
+              </div>
+            </div>
+          </form>
           <div className='column'>
-            <button>Save on Spotify</button>
-            <div className='column is-size-7 has-text-centered has-text-grey-light'>
+            <button type='submit' >Save on Spotify</button>
+            <div className='is-size-7 has-text-centered has-text-grey-light'>
               You will connect with you Spotify account to save your playlist. 
               The playlist will be reproduced on Spotify. We are not a streaming service.
             </div>
